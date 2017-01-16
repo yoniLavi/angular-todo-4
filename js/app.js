fc29ea7b4e3b61bfd66afe4b4d31718f938bfbc2
MrBim <bimtech84@gmail.com>
2017-01-16 14:38:47 +0000

    tweeking routing

--- js/app.js
+++ js/app.js
@@ -1,14 +1,14 @@
 angular.module('TodoApp', ['ngRoute', 'RouteControllers']);
- 
+
 angular.module('TodoApp').config(function($locationProvider, $routeProvider) {
-    $locationProvider.html5Mode(true);  // Enable href routing without hashes
- 
-    $routeProvider.when('/', {
-        templateUrl: 'templates/home.html',
-        controller: 'HomeController'
-    })
-    .when('/accounts/register', {
-        templateUrl: 'templates/register.html',
-        controller: 'RegisterController'
-    });
+	$locationProvider.html5Mode(true);
+	
+	$routeProvider.when('/', {
+		templateUrl: 'templates/home.html',
+		controller: 'HomeController'
+	})
+	.when('/accounts/register', {
+		templateUrl: 'templates/register.html',
+		controller: 'RegisterController'
+	});
 });
